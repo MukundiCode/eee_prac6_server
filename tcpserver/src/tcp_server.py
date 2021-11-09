562f8b242d6459a1840d7058b5a271364e0827d7
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-11-09 15:32:37 +0200

    sending now works

--- tcpserver/src/tcp_server.py
+++ tcpserver/src/tcp_server.py
@@ -1,29 +1,85 @@
 import socket
 import sys
+import threading
+from flask import Flask, render_template
 
 f = open("/data/sensorlog.txt", "w+")
 
 
+#establishing connection
+global conn
+conn = None
 TCP_IP = '0.0.0.0'
 TCP_PORT = 5003
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-
 newconnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 newconnection.bind((TCP_IP, TCP_PORT))
 newconnection.listen(1)
-
 conn, addr = newconnection.accept()
-print('Connection address:', addr)
-
-#TODO: Add code to create a socket ready to recieve data
 
 
+#method for handling data reception
+def getData(newconnection):
+    #conn, addr = newconnection.accept()
+    print('Connection address:', addr)
+    with newconnection:
+        while True:
+            data = conn.recv(1024).decode()
+            print('\nReceived', data)
+            f.write("\nReceived: ")
+            f.write(data)
+            contents = f.read()
+            print(contents)
+            f.flush()
+            if not data: break
 
-with newconnection:
-    while True:
-        data = conn.recv(1024)
-        print('\nReceived', repr(data))
-        f.write("\nReceived: ")
-        f.write(repr(data))
-        f.flush()
-        if not data: break
+
+#interface stuff
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+@app.route('/SensorStream')
+def sensor_stream():
+    #TODO: Add code that displays the contents of log file /data/sensorlog.txt 
+    return "sensor"
+
+@app.route('/download')
+def download_file():
+    #TODO: Add code to download the file /data.sensorlog.txt
+    return "download"
+
+@app.route('/sendon')
+def sendon():
+    print("sendon")
+    data = "sendon"
+    conn.send(data.encode())
+    return index()
+
+@app.route('/sendoff')
+def sendoff():
+    print("sendoff")
+    data = "sendoff"
+    conn.send(data.encode())
+    return index()
+
+@app.route('/status')
+def status():
+    return "status"
+
+@app.route('/log')
+def log():
+    f = open("sensorlog.txt", "r+")
+    for i in f :
+        print(i)
+    return "log"
+
+@app.route('/exit')
+def exit():
+    return "exit"
+
+if __name__ == '__main__':
+    threading.Thread(target=lambda: app.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)).start()
+    getData(newconnection=newconnection)
--- webserver/src/webserver.py
+++ webserver/src/webserver.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_file, render_template
+import socket
+import sys
 
-app = Flask(__name__)
+
+app = Flask("name")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -16,6 +19,31 @@ def download_file():
     #TODO: Add code to download the file /data.sensorlog.txt
     return "download"
 
+@app.route('/sendon')
+def sendon():
+
+    return "sendon"
+
+@app.route('/sendoff')
+def sendoff():
+    return "sendoff"
+
+@app.route('/status')
+def status():
+    return "status"
+
+@app.route('/log')
+def log():
+    f = open("sensorlog.txt", "r+")
+    for i in f :
+        print(i)
+    return "log"
+
+@app.route('/exit')
+def exit():
+    return "exit"
+
+
 
 
 #TODO Add the remaining functions requested either by adding more pages to the template or get fancy with more templates and better formatting
@@ -23,6 +51,6 @@ def download_file():
 
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=5080, debug=True) #Use this line to test basic functionality locally before trying to deploy on Pi
-    app.run(host='0.0.0.0', port=80)
+    #app.run(host='0.0.0.0', port=80)
